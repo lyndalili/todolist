0f0cc567da720aaabc8b48a9dadda830a33c6d8e
WadeTheDev <echo1006j@gmail.com>
2022-07-07 11:43:47 +0200

    add taks to list function

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 const taskInput = document.querySelector('.container form input')
 const addTask = document.getElementById('add')
+const form = document.getElementById('usrform')
+const taskBox = document.getElementById('taskbox')
+const taskNameStyle = document.getElementById('taskname')
+const checkButton = document.querySelector('.check')
+const closeButton = document.querySelector('.close')
+const doingButton = document.querySelector('.doing')
+const modfiyButton = document.querySelector('.modify')
+const allSelection = document.getElementById('all')
+const toDoSeletion = document.getElementById('todo')
+const doingSelection = document.getElementById('doing')
+const doneSelection = document.getElementById('done')
+let taskAll = []
+let taskToDo = []
+let taskDone = []
+let taskDoing = []
 
-addTask.addEventListener('click', () => {
+
+// AJOUTER UNE TACHE DANS LA DOM
+
+const onTaskSubmit = (e) => {
+        e.preventDefault()
         let taskName = taskInput.value;
-        
-})
+        let tasks = {name : taskName, status : "to do"}
+        taskAll.push(tasks)
+        taskBox.innerHTML += `
+        <li class="task"><button class="check"></button>
+        <button class="doing"></button>
+        <p id="taskname">${taskName}</p>
+        <button class="close"></button>
+        <button class="modify"></button>
+        </li>
+        `
+        console.log(taskAll)
+}
+
+
+form.addEventListener('submit', onTaskSubmit);
+
+// if (tasks.status === "done") {
+//         taskDone.push(tasks)
+// }
+// if (tasks.status === "doing") {
+//         taskDoing.push(tasks)
+// }
