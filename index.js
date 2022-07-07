3827a8bf7fada7a03060bdd14d4fb14977b07c4b
WadeTheDev <echo1006j@gmail.com>
2022-07-07 09:21:33 +0200

    icon navigator

---
+++ index.js
@@ -0,0 +1,7 @@
+const taskInput = document.querySelector('.container form input')
+const addTask = document.getElementById('add')
+
+addTask.addEventListener('click', () => {
+        let taskName = taskInput.value;
+        
+})
